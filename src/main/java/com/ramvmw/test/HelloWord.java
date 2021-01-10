f5acfa9f723dcbf2ff38eef628713ed9564fdf74
unknown <ramvm@hotmail.com>
2021-01-10 13:25:36 -0600

    Java Project Test with LocalDateTime

--- src/main/java/com/ramvmw/test/HelloWord.java
+++ src/main/java/com/ramvmw/test/HelloWord.java
@@ -1,9 +1,39 @@
 package com.ramvmw.test;
 
+import java.time.LocalDateTime;
+
 public class HelloWord
 {
     public static void main(String args [])
     {
+        HelloWord helloWord;
+
+        helloWord = new HelloWord();
+
         System.out.println("Hello word!");
+
+        helloWord.Greeting();
+    }
+
+    private void Greeting()
+    {
+        LocalDateTime time;
+        int hour;
+
+        time = LocalDateTime.now();
+        hour = time.getHour();
+
+        if(hour >= 0 && hour <= 11)
+        {
+            System.out.println("Good afternoon");
+        }
+        else if(hour >= 12 && hour <= 17)
+        {
+            System.out.println("Good afternoon");
+        }
+        else if(hour >= 18 && hour <= 23)
+        {
+            System.out.println("Good evening");
+        }
     }
 }
